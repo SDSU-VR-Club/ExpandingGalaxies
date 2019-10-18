3462f48c7829f4bdea7e140d72d7380673602aaf
Patrick <perrinepatrick@gmail.com>
2019-10-18 15:25:48 -0700

    added Redshift script and shader

---
+++ Assets/Scripts/RedShift.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class RedShift : MonoBehaviour
+{
+	Material RedshiftMat;
+
+	Camera cam;
+
+	public void Start(){
+	}
+
+	private void OnRenderImage(RenderTexture src, RenderTexture dst)
+	{
+		if (RedshiftMat == null) {
+			RedshiftMat = new Material(Shader.Find("Hidden/RedShift"));
+		}
+
+		if(cam == null){
+			cam = this.GetComponent<Camera>();
+            cam.depthTextureMode = cam.depthTextureMode | DepthTextureMode.Depth;
+		}
+
+		Graphics.Blit(src, dst, RedshiftMat);
+	}
+}
