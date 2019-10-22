1cb8c3a318894c8d3b84933883956f23857240a5
Byron <darkforcesii@gmail.com>
2019-10-22 14:06:37 -0700

    Added Audio
    I added a script that will let us play SFX whenever something is selected. I also added in Nathaniel's song to Patrick's scene.

---
+++ Assets/Scripts/AudioScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioScript : MonoBehaviour
+{
+    public AudioClip UI_Select;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void PlaySFX()
+    {
+        GetComponent<AudioSource>().PlayOneShot(UI_Select);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlaySFX();
+        }
+    }
+}
