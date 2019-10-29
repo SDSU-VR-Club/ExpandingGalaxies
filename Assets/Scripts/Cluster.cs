630af617e6eab4478cd9474913874d21c5dc2e72
Carter Andrews <andrewscart@gmail.com>
2019-10-28 18:33:39 -0700

    Cluster Generator modifications
    modified generator script to have inner/outer radius, cluster prefabs, cluster spawns in parent, and adjustable cluster count

---
+++ Assets/Scripts/Cluster.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//cluster
+public class Cluster : MonoBehaviour
+{
+    private static Vector3 currentCord, prevCord;
+
+    public static void PopulateCluster(GameObject cluster, GameObject planet, int seed, int amountOfPlanets, Vector3 clusterNoiseValue, int j, float clusterScalar)
+    {
+        //initialize the psuedorando-generator with user seed
+        Random.InitState(seed + j);
+
+        //create first planet inside cluster
+        currentCord = clusterNoiseValue + (Random.insideUnitSphere * (clusterScalar / 2));
+        GameObject newPlanet = Instantiate(planet, cluster.transform, true);
+        newPlanet.transform.position = currentCord;
+
+        //populate cluster without colliding planets 
+        currentCord = clusterNoiseValue + (Random.insideUnitSphere * (clusterScalar / 2));
+        while (amountOfPlanets >= 0)
+        {
+            Collider[] colliders = Physics.OverlapSphere(currentCord, (planet.transform.localScale.x / 2), layerMask: 0, queryTriggerInteraction: QueryTriggerInteraction.Collide);
+            if (colliders.Length == 0)
+            {
+                newPlanet = Instantiate(planet, cluster.transform, true);
+                newPlanet.transform.position = currentCord;
+                amountOfPlanets--;
+                currentCord = clusterNoiseValue + (Random.insideUnitSphere * (clusterScalar / 2));
+            }
+        }
+    }
+}
--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -2,113 +2,95 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//generator
+//Creates a Universe object that spawns a certain configuration of cluster objects with specified paramters
 public class Generator : MonoBehaviour
 {
+    //controls the random elements of universe creation
     public int seed = 1;
+    //universeRadiusScalar controls the size of clusters
+    public float clusterRadiusScalar = 1.0f;
+    //Clusters spawn in between innerRadius and outerRadius
+    public float innerRadius = 10.0f;
+    public float outerRadius = 200.0f;
 
-    public float universeRadiusScalar = 1.0f;
+    //the desired number of clusters to spawn
+    public int clusterCount;
 
-    public float playerRadius = 10.0f;
-
-    public GameObject planet;
+    //public GameObject planet;
+    //object prefab to spawn in universe
     public GameObject cluster;
 
+    [HideInInspector]
+    public Universe currentUniverse;
     void Start()
     {
-        Universe testUniverse = new Universe(cluster, planet, seed, universeRadiusScalar, playerRadius);
+        Universe testUniverse = new Universe(cluster, seed, clusterRadiusScalar, innerRadius, outerRadius,clusterCount);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-    }
 
 }
 
-//cluster
-public class Cluster : MonoBehaviour
-{
-    private static Vector3 currentCord, prevCord;
 
-    public static void PopulateCluster(GameObject cluster, GameObject planet, int seed, int amountOfPlanets, Vector3 clusterNoiseValue, int j, float clusterScalar)
-    {
-        //initialize the psuedorando-generator with user seed
-        Random.InitState(seed + j);
-
-        //create first planet inside cluster
-        currentCord = clusterNoiseValue + (Random.insideUnitSphere * (clusterScalar / 2));
-        GameObject newPlanet = Instantiate(planet, cluster.transform, true);
-        newPlanet.transform.position = currentCord;
-
-        //populate cluster without colliding planets 
-        currentCord = clusterNoiseValue + (Random.insideUnitSphere * (clusterScalar / 2));
-        while(amountOfPlanets >= 0)
-        {
-            Collider[] colliders = Physics.OverlapSphere(currentCord, (planet.transform.localScale.x / 2), layerMask: 0, queryTriggerInteraction: QueryTriggerInteraction.Collide);
-            if (colliders.Length == 0)
-            {
-                newPlanet = Instantiate(planet, cluster.transform, true);
-                newPlanet.transform.position = currentCord;
-                amountOfPlanets--;
-                currentCord = clusterNoiseValue + (Random.insideUnitSphere * (clusterScalar / 2));
-            }
-        }
-    }
-}
 
 //universe
 public class Universe : MonoBehaviour
 {
-    private Vector3 currentCord, prevCord;
-    private float clusterScaleScalar = 20;
-    private int planetAmount = 20;
+    //private Vector3 currentCord, prevCord;
+    //private float clusterScaleScalar = 20;
+    //private int planetAmount = 20;
 
     //constructor that generates all game objects within the universe (universe->clusters->(stars?) in clusters)
-    public Universe(GameObject cluster, GameObject planet, int seed, float uniScalar, float playerRadius)
+    public Universe(GameObject cluster, int seed, float clusterScalar, float innerRadius,float outerRadius,int clusterCount)
     {
+        //Create the transform that will hold the clusters
+        GameObject clusterParent = new GameObject();
+        clusterParent.name = "clusters";
         //create the seed as per user input seed value ( same seed = same generation )
         Random.InitState(seed);
 
         //players radius surrounding them (area of no spawn for clusters)
-        Vector3 playerRadiusVector = new Vector3(playerRadius, playerRadius, playerRadius);
-        Debug.Log(playerRadiusVector);
-        int failureCount = 0, indexer = 1;
+        //Vector3 playerRadiusVector = new Vector3(playerRadius, playerRadius, playerRadius);
+        //Debug.Log(playerRadiusVector);
+        int failSafeCount = 0, currentSpawn = 1;
 
         //a failure is defined as a collider spawning inside of another collider
-        while (failureCount < 50)
+        //run until an arbitrary limit has been reached or the requiste number of clusters has been spawned
+        while (failSafeCount < 10000&&currentSpawn<clusterCount+1)
         {
             //generate current random coord
-            this.currentCord = Random.insideUnitSphere * uniScalar;
-            Debug.Log(this.currentCord);
+            float randomDistance = Random.Range(innerRadius, outerRadius);
+            Vector3 spawnPos = Random.insideUnitSphere.normalized *randomDistance ;
+            
 
             //IF there are colliders within the randomly generated coord, with radius of cluster : (clusterScale / 2)
-            Collider[] colliders = Physics.OverlapSphere(this.currentCord, (clusterScaleScalar / 2), layerMask: 0, queryTriggerInteraction: QueryTriggerInteraction.Collide); //get all colliders within this pos and r
+            Collider[] colliders = Physics.OverlapSphere(spawnPos, (clusterScalar / 2), layerMask: 0, queryTriggerInteraction: QueryTriggerInteraction.Collide); //get all colliders within this pos and r
             //OR, if the coordinate is inside of the "player viewing radius"
             //OR, if the previous coordinates magnitude is within 2 radius' (radiuses?? idfkgatdamn) of the current coord (FOR good measure)
             // THEN dont create clluster here
             if ((colliders.Length > 0)
-                || (currentCord.magnitude < playerRadiusVector.magnitude) 
-                || (Mathf.Abs(currentCord.magnitude - prevCord.magnitude) < (clusterScaleScalar/2)) 
-                || (Mathf.Abs(prevCord.magnitude - currentCord.magnitude) < (clusterScaleScalar / 2)))
+                //|| (currentCord.magnitude < playerRadius)
+                //|| (Mathf.Abs(currentCord.magnitude - prevCord.magnitude) < (clusterScaleScalar/2)) 
+                //|| (Mathf.Abs(prevCord.magnitude - currentCord.magnitude) < (clusterScaleScalar / 2)))
+                )
             {
-                ++failureCount;
-                Debug.Log("Failure: " + Physics.OverlapSphere(currentCord, (clusterScaleScalar / 2)).Length + " --COLLISION-- " + currentCord);
-                Debug.Log("Failure count: " + failureCount + "Generating new..");
+                ++failSafeCount;
+                Debug.Log("Failure: " + Physics.OverlapSphere(spawnPos, (clusterScalar / 2)).Length + " --COLLISION-- " + spawnPos);
+                Debug.Log("Failure count: " + failSafeCount + "Generating new..");
             }
             //else ,generate new cluster, scale by scalar, populate with populatecluster method
             else
             {
-                GameObject newCluster = Instantiate(cluster, currentCord, Quaternion.identity);
-                newCluster.name = "Cluster" + (indexer);
-                newCluster.transform.localScale *= clusterScaleScalar;
+                //create a new cluster at spawnPos with random location
+                GameObject newCluster = Instantiate(cluster, spawnPos, Random.rotation,clusterParent.transform);
+                newCluster.name = "Cluster" + currentSpawn;
+                newCluster.transform.localScale *= clusterScalar;
 
                 //populate generated cluster
-                Cluster.PopulateCluster(newCluster, planet, seed, planetAmount, currentCord, indexer, clusterScaleScalar);
+                //Cluster.PopulateCluster(newCluster, planet, seed, planetAmount, currentCord, indexer, clusterScaleScalar);
 
-                prevCord = currentCord;
-                Debug.Log("Cluster " + (indexer) + ": " + newCluster.transform.position);
-                ++indexer;
+                //prevCord = currentCord;
+                Debug.Log("Cluster " + (currentSpawn) + ": " + newCluster.transform.position);
+                ++currentSpawn;
             }
         }
     }
