a6d2b960796682a7c56cc09962ca9818a8577852
Kainkun <kainkun100@gmail.com>
2019-10-28 20:13:08 -0700

    star expansion 2d and 3d

---
+++ Assets/Scripts/Expansion.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Expansion : MonoBehaviour
+{
+    Transform player;
+    public float speed = 1;
+    public float acceleration = 0;
+    public GameObject starPrefab;
+    public float randomRange = 10;
+    public bool randomStars;
+    public int starCount = 100;
+    public List<Transform> stars = new List<Transform>();
+
+
+    void Start()
+    {
+        player = transform;
+
+        if (randomStars)
+            for (int i = 0; i < starCount; i++)
+            {
+                stars.Add(Instantiate(starPrefab, new Vector3(Random.Range(-randomRange, randomRange), Random.Range(-randomRange, randomRange), Random.Range(-randomRange, randomRange)), Quaternion.identity).transform);
+            }
+    }
+
+
+    void Update()
+    {
+        speed += acceleration * Time.deltaTime;
+
+        foreach (Transform star in stars)
+        {
+            star.position += (star.position - player.position) * speed * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/ExpansionTest2D.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpansionTest2D : MonoBehaviour
+{
+    public Transform player;
+    public float speed = 1;
+    public GameObject starPrefab;
+    public bool randomStars;
+    public List<Transform> stars = new List<Transform>();
+
+
+    void Start()
+    {
+
+        if(randomStars)
+        for (int i = 0; i < 100; i++)
+        {
+            stars.Add( Instantiate(starPrefab, new Vector2(Random.Range(-5f,5f), Random.Range(-5f, 5f)), Quaternion.identity).transform );
+        }
+    }
+
+
+    void Update()
+    {
+        foreach (Transform star in stars)
+        {
+            star.position += (star.position - player.position) * speed * Time.deltaTime;
+        }
+    }
+}
