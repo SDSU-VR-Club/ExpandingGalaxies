1eb963e8a04d2cfd7b806bda936caa248c740ca7
Carter <andrewscart@gmail.com>
2019-10-31 18:57:36 -0700

    added teleport prefab to make controllers work

---
+++ Assets/Scripts/LaserPointer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserPointer : MonoBehaviour
+{
+    LineRenderer lr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3[] vect = { transform.position, transform.forward*100000 };
+        lr.SetPositions(vect);
+    }
+}
