f712764bc7056878f191778ce81f048fc456b0d8
Jacob <jacob@goldenneedham.com>
2020-02-08 02:09:46 -0800

    Git Chunk scaling working!!

---
+++ Assets/Scripts/chunking/Chunk.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Chunk : MonoBehaviour
+{
+    public int seed;
+    Transform[] clusters;
+    
+    //Size is a function of Time
+    public Vector3 size = Vector3.zero;
+    
+    public Vector3Int chunk;
+
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+    }
+
+    public virtual void UpdateChunk(float time){
+        //Use Preferred RNG method here
+        size = new Vector3(time, time, time);
+    }
+
+    void GenerateChunk(){
+        //Do setup for the clusters
+    }
+
+    void OnDrawGizmos(){
+        Gizmos.DrawWireCube(this.transform.position, size);
+    }
+}
---
+++ Assets/Scripts/chunking/ChunkManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkManager : MonoBehaviour
+{
+    private int _renderDistance = 1;
+    
+    public int renderDistance{
+        get{ return _renderDistance; }
+
+        set{
+            _renderDistance = value;    
+            int shellNum = renderDistance * 2 + 1;
+            chunks = new Chunk[shellNum, shellNum, shellNum];
+        }
+    }
+
+    public float time = 1;
+
+    Chunk[,,] chunks;
+    public Vector3Int currentChunk;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int shellNum = renderDistance * 2 + 1;
+        chunks = new Chunk[shellNum, shellNum, shellNum];
+        GenerateChunks();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateChunks();
+    }
+
+    void UpdateChunks(){
+        for(int i = -renderDistance; i <= renderDistance; i++){
+            for(int j = -renderDistance; j <= renderDistance; j++){
+                for(int k = -renderDistance; k <= renderDistance; k++){
+                    int ii = i + renderDistance, ij = j + renderDistance, ik = k + renderDistance;
+                    chunks[ii, ij, ik].transform.position = new Vector3(i, j, k) * time;
+                    chunks[ii, ij, ik].UpdateChunk(time);
+                }
+            }
+        }
+    }
+
+    void GenerateChunks(){
+        for(int i = -renderDistance; i <= renderDistance; i++){
+            for(int j = -renderDistance; j <= renderDistance; j++){
+                for(int k = -renderDistance; k <= renderDistance; k++){
+                    int ii = i + renderDistance, ij = j + renderDistance, ik = k + renderDistance;
+
+                    GameObject go = new GameObject();
+                    Chunk chunk = go.AddComponent<Chunk>();
+                    chunks[ii, ij, ik] = chunk;
+                    go.transform.position = new Vector3(i, j, k);
+                    chunk.chunk = new Vector3Int(i, j, k);
+                    go.transform.parent = this.transform;
+                }
+            }
+        }
+    }
+}
